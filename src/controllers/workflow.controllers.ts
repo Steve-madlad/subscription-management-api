62704a5ed0f1ca89fc9cddbf4c5550d7c35ea808
Estifanos Wendimu <teffsauce2@gmail.com>
2025-10-13 20:45:56 +0300

    debugging development

--- src/controllers/workflow.controllers.ts
+++ src/controllers/workflow.controllers.ts
@@ -1,94 +1,92 @@
-// import type { WorkflowContext } from "@upstash/workflow";
-// import { createRequire } from "module";
-// const requiree = createRequire(import.meta.url);
-// const { serve } = requiree("@upstash/workflow/express");
+import type { WorkflowContext } from "@upstash/workflow";
+import { createRequire } from "module";
+const required = createRequire(import.meta.url);
+const { serve } = required("@upstash/workflow/express.js");
 
-// import dayjs, { type Dayjs } from "dayjs";
-// import Subscription, {
-//   SubscriptionDocument,
-// } from "../models/subscription.model.js";
-// import sendReminderEmail from "../utils/send-reminder-email.js";
+import dayjs, { type Dayjs } from "dayjs";
+import Subscription, {
+  SubscriptionDocument,
+} from "../models/subscription.model.js";
+import sendReminderEmail from "../utils/send-reminder-email.js";
 
 const reminders = [10, 7, 5, 2, 1];
 
-// export const sendReminders = serve(
-//   // async (context: WorkflowContext<unknown>) => {
-//   //   const { subscriptionId } = context.requestPayload as Record<string, string>;
+export const sendReminders = serve(
+  async (context: WorkflowContext<unknown>) => {
+    const { subscriptionId } = context.requestPayload as Record<string, string>;
 
-//   //   const subscription = await fetchSubscription(context, subscriptionId);
+    const subscription = await fetchSubscription(context, subscriptionId);
 
-//   //   if (!subscription || subscription.status !== "active") return;
+    console.log(subscription);
 
-//   //   const renewalDate = dayjs(subscription.renewalDate);
+    if (!subscription || subscription.status !== "active") return;
 
-//   //   if (renewalDate.isBefore(dayjs())) {
-//   //     console.log(
-//   //       `RenewalDate has expired for user ${subscription.user} with subscription id ${subscriptionId}. Exiting Workflow...`
-//   //     );
-//   //     return;
-//   //   }
+    const renewalDate = dayjs(subscription.renewalDate);
 
-//   //   for (const daysBefore of reminders) {
-//   //     const reminderDate = renewalDate.subtract(daysBefore, "day");
+    if (renewalDate.isBefore(dayjs())) {
+      console.log(
+        `RenewalDate has expired for user ${subscription.user} with subscription id ${subscriptionId}. Exiting Workflow...`
+      );
+      return;
+    }
 
-//   //     if (reminderDate.isAfter(dayjs())) {
-//   //       await sleepUntilReminder(
-//   //         context,
-//   //         `reminder ${daysBefore} days before`,
-//   //         reminderDate
-//   //       );
-//   //     }
+    for (const daysBefore of reminders) {
+      const reminderDate = renewalDate.subtract(daysBefore, "day");
 
-//   //     await triggerReminder(
-//   //       context,
-//   //       `reminder ${daysBefore} days before`,
-//   //       subscription
-//   //     );
-//   //   }
-//   // }
-// );
+      if (reminderDate.isAfter(dayjs())) {
+        await sleepUntilReminder(
+          context,
+          `reminder ${daysBefore} days before`,
+          reminderDate
+        );
+      }
 
-export const sendReminders = () => {
-  console.log("sendReminders workflow triggered");
-}
+      await triggerReminder(
+        context,
+        `reminder ${daysBefore} days before`,
+        subscription
+      );
+    }
+  }
+);
 
-// type PartialUser = { name: string; email: string };
+type PartialUser = { name: string; email: string };
 
-// export type SubscriptionWithPartialUser = Omit<SubscriptionDocument, "user"> & {
-//   user: PartialUser;
-// };
+export type SubscriptionWithPartialUser = Omit<SubscriptionDocument, "user"> & {
+  user: PartialUser;
+};
 
-// const fetchSubscription = async (
-//   context: WorkflowContext,
-//   subscriptionId: string
-// ): Promise<SubscriptionWithPartialUser | null> => {
-//   return await context.run("get subscription", async () => {
-//     return (await Subscription.findById(subscriptionId).populate(
-//       "user",
-//       "name email"
-//     )) as SubscriptionWithPartialUser | null;
-//   });
-// };
+const fetchSubscription = async (
+  context: WorkflowContext,
+  subscriptionId: string
+): Promise<SubscriptionWithPartialUser | null> => {
+  return await context.run("get subscription", async () => {
+    return (await Subscription.findById(subscriptionId).populate(
+      "user",
+      "name email"
+    )) as SubscriptionWithPartialUser | null;
+  });
+};
 
-// const sleepUntilReminder = async (
-//   context: WorkflowContext,
-//   label: string,
-//   date: Dayjs
-// ) => {
-//   console.log(`Sleeping until ${label} reminder at ${date}`);
-//   await context.sleepUntil(label, date.toDate());
-// };
+const sleepUntilReminder = async (
+  context: WorkflowContext,
+  label: string,
+  date: Dayjs
+) => {
+  console.log(`Sleeping until ${label} reminder at ${date}`);
+  await context.sleepUntil(label, date.toDate());
+};
 
-// const triggerReminder = async (
-//   context: WorkflowContext,
-//   label: string,
-//   subscription: SubscriptionWithPartialUser
-// ) => {
-//   return await context.run(label, async () => {
-//     console.log(`Triggering ${label} subscription end`);
-//     await sendReminderEmail({
-//       to: subscription.user.email,
-//       subscription: subscription,
-//     });
-//   });
-// };
+const triggerReminder = async (
+  context: WorkflowContext,
+  label: string,
+  subscription: SubscriptionWithPartialUser
+) => {
+  return await context.run(label, async () => {
+    console.log(`Triggering ${label} subscription end`);
+    await sendReminderEmail({
+      to: subscription.user.email,
+      subscription: subscription,
+    });
+  });
+};
--- src/utils/send-reminder-email.ts
+++ src/utils/send-reminder-email.ts
@@ -1,38 +1,38 @@
-// import dayjs from "dayjs";
-// import transporter, { accountInfo } from "../config/nodemailer.js";
-// import { SubscriptionWithPartialUser } from "../controllers/workflow.controllers.js";
-// import { generateEmailTemplate } from "./email-templates.js";
+import dayjs from "dayjs";
+import transporter, { accountInfo } from "../config/nodemailer.js";
+import { SubscriptionWithPartialUser } from "../controllers/workflow.controllers.js";
+import { generateEmailTemplate } from "./email-templates.js";
 
-// export const sendReminderEmail = async ({
-//   to,
-//   subscription,
-// }: {
-//   to: string;
-//   subscription: SubscriptionWithPartialUser;
-// }) => {
-//   if (!to) throw new Error("Missing required parametres for email");
+export const sendReminderEmail = async ({
+  to,
+  subscription,
+}: {
+  to: string;
+  subscription: SubscriptionWithPartialUser;
+}) => {
+  if (!to) throw new Error("Missing required parametres for email");
 
-//   const mailInfo = {
-//     name: subscription.user.name,
-//     subscriptionName: subscription.name,
-//     subscriptionType: subscription.frequency,
-//     subscriptionPrice: subscription.price,
-//     daysLeft: dayjs(subscription.renewalDate).diff(dayjs(), "day"),
-//   };
+  const mailInfo = {
+    name: subscription.user.name,
+    subscriptionName: subscription.name,
+    subscriptionType: subscription.frequency,
+    subscriptionPrice: subscription.price,
+    daysLeft: dayjs(subscription.renewalDate).diff(dayjs(), "day"),
+  };
 
-//   const template = generateEmailTemplate(mailInfo);
+  const template = generateEmailTemplate(mailInfo);
 
-//   const mailOptions = {
-//     from: accountInfo,
-//     to,
-//     subject: template.subject,
-//     html: template.body,
-//   };
+  const mailOptions = {
+    from: accountInfo,
+    to,
+    subject: template.subject,
+    html: template.body,
+  };
 
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) return console.log(error, "Error sending email");
-//     console.log("Email Sent" + info.response);
-//   });
-// };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) return console.log(error, "Error sending email");
+    console.log("Email Sent" + info.response);
+  });
+};
 
-// export default sendReminderEmail;
+export default sendReminderEmail;

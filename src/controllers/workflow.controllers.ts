e9f2a8623b40f947adb6efc98e14a53d9914d1a1
Estifanos Wendimu <teffsauce2@gmail.com>
2025-10-13 21:06:24 +0300

    debugging

--- src/controllers/workflow.controllers.ts
+++ src/controllers/workflow.controllers.ts
@@ -2,7 +2,7 @@ import type { WorkflowContext } from "@upstash/workflow";
 // import { createRequire } from "module";
 // const required = createRequire(import.meta.url);
 // const { serve } = required("@upstash/workflow/express.js");
-const { serve } = require("@upstash/workflow/express");
+const { serve } = require("@upstash/workflow/express.cjs");
 
 
 import dayjs, { type Dayjs } from "dayjs";

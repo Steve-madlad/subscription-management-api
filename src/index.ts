451d08b2b1fd91e37c2b6813c0202ac5d4624632
Estifanos Wendimu <teffsauce2@gmail.com>
2026-02-27 23:04:32 +0300

    minor CORS fix

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import dayjs from "dayjs";
 const app = express();
 app.use(
   cors({
-    origin: "http://swagster.vercel.app",
+    origin: "https://swagster.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     exposedHeaders: ["Authorization"],
